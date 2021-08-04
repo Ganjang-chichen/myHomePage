f3c63ff9c301c5aa487f0c7f2c9b5a0d513e6c69
Ganjang-chichen <59672593+Ganjang-chichen@users.noreply.github.com>
2021-08-04 17:10:59 +0900

    ip 추적

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+  console.log(ip);
   res.render('index', { title: 'Express' });
 });
 
 router.get('/raindrop', function(req, res, next) {
+  const ip = req.headers['x-forwarded-for'] ||  req.connection.remoteAddress;
+  console.log(ip);
   res.render('raindrop', { title: 'Express' });
 });
 
